f7090269ee887e86c7b641dc92cac975714e8470
Pereyra Jose <pereyrajose848@gmail.com>
2023-03-21 21:10:27 -0300

    configure peticion post y logre agregar a base de datos, generare un par de cliente para proceder con el get

--- src/controllers/clientes.controllers.js
+++ src/controllers/clientes.controllers.js
@@ -1,10 +1,24 @@
-
+import cliente from "../models/cliente"
 
 export const listarClientes =(req,res)=>{
     res.send("Aqui devolveremos un arreglo con los clientes")
 };
 
-export const agregarCliente= (req,res)=>{
-    console.log(req.body)
-    res.send("Esto es una prueba de la peticion POST")
+export const agregarCliente= async(req,res)=>{
+    try {
+        console.log(req.body);
+        const clienteNuevo= new cliente(req.body);
+        await clienteNuevo.save();
+        res.status(201).json({
+            mensaje: "el cliente fue agrergado con exito"
+        })
+
+        
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({
+            mensaje:"Error al intentar agregar"
+        })
+    }
+   
 };
---
+++ src/models/cliente.js
@@ -0,0 +1,40 @@
+import mongoose, {Schema} from "mongoose";
+
+const clienteSchema = new Schema({
+    Nombre:{
+        type:String,
+        required:true,
+        minLenght:2,
+        maxLenght:50
+    },
+    Operacion:{
+        type:String,
+        required:true,
+        
+    },
+    Monto:{
+        type:Number,
+        required:true,
+        min:1,
+        max:10000
+    },
+    Fecha:{
+        type:String,
+        required:true,
+
+    },
+    Hora:{
+        type:String,
+        required:true,
+
+    },
+    Operador:{
+        type:String,
+        required:true,
+        minLenght:2,
+        maxLenght:20
+    }
+})
+
+const cliente = mongoose.model("cliente",clienteSchema);
+export default cliente
