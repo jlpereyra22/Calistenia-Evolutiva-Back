499021663149f235d458e9beb99536a0fd8e9c25
Pereyra Jose <pereyrajose848@gmail.com>
2023-03-21 21:26:09 -0300

    peticion get configurada y obtenes datos de la bd

--- src/controllers/clientes.controllers.js
+++ src/controllers/clientes.controllers.js
@@ -1,7 +1,17 @@
 import cliente from "../models/cliente"
 
-export const listarClientes =(req,res)=>{
-    res.send("Aqui devolveremos un arreglo con los clientes")
+export const listarClientes = async(req,res)=>{
+   try {
+    const listaCliente= await cliente.find();
+    res.status(200).json(listaCliente);
+    
+   } catch (error) {
+    console.log(error);
+    res.status(404).json({
+        mensaje: "Error al obtener la base de datos"
+    })
+
+   }
 };
 
 export const agregarCliente= async(req,res)=>{
