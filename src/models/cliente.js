0a493fbd8e8fcd83b269d0fe1cd219b698ce2fff
Pereyra Jose <pereyrajose848@gmail.com>
2023-03-21 21:21:51 -0300

    arreglamos un error con la plantilla del model de cliente

--- src/models/cliente.js
+++ src/models/cliente.js
@@ -1,40 +1,53 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
 const clienteSchema = new Schema({
-    Nombre:{
-        type:String,
-        required:true,
-        minLenght:2,
-        maxLenght:50
-    },
-    Operacion:{
-        type:String,
-        required:true,
-        
-    },
-    Monto:{
-        type:Number,
-        required:true,
-        min:1,
-        max:10000
-    },
-    Fecha:{
-        type:String,
-        required:true,
+  Nombre: {
+    type: String,
+    required: true,
+    minLenght: 2,
+    maxLenght: 50,
+  },
+  Dni: {
+    type: Number,
+    required: true,
+    unique: true,
+    minLenght: 1,
+    maxLenght: 12,
+  },
+  Telefono: {
+    type: Number,
+    required: true,
+    minLenght: 5,
+    maxLenght: 25,
+  },
+  Mail: {
+    type: String,
+    required: true,
+    unique: true,
+    minLenght: 1,
+    maxLenght: 50,
+  },
+  FNacimiento: {
+    type: String,
+    required: true
+  },
 
-    },
-    Hora:{
-        type:String,
-        required:true,
+  Estado: {
+    type: String,
+    required: true
+  },
+  FPago: {
+    type: String,
+    required: true
+  },
+  Monto: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 10000
+  }
 
-    },
-    Operador:{
-        type:String,
-        required:true,
-        minLenght:2,
-        maxLenght:20
-    }
-})
+});
 
-const cliente = mongoose.model("cliente",clienteSchema);
-export default cliente
+const cliente = mongoose.model("cliente", clienteSchema);
+export default cliente;
