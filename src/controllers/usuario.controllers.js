5bebd897318f3721944b453889516f49b8ef3389
Pereyra Jose <pereyrajose848@gmail.com>
2023-05-02 21:17:40 -0300

    generamos rutas y controllers para editar y eliminar usuarios, resta hacer la logica del front y generar rutas protegidas para que discrimine empleados de maestros

--- src/controllers/usuario.controllers.js
+++ src/controllers/usuario.controllers.js
@@ -14,19 +14,18 @@ export const listarUsuarios = async (req, res) => {
 };
 
 export const agregarUsuario = async (req, res) => {
-  try {
-    console.log(req.body);
-    const usuarioNuevo = new usuario(req.body);
-    await usuarioNuevo.save();
-    res.status(201).json({
-      mensaje: "el usuario fue agrergado con exito",
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(400).json({
-      mensaje: "Error al intentar agregar",
-    });
-  }
+try {
+  const usuarioNuevo= new usuario(req.body);
+  await usuarioNuevo.save();
+  res.status(201).json({
+    mensaje:"El Usuario fue correctamente creado"
+  })
+} catch (error) {
+  console.log( error);
+  res.status(400).json({
+    mensaje:"Error al agregar un usuario"
+  })
+}
 };
 
 export const login = async (req, res) => {
@@ -63,7 +62,7 @@ export const login = async (req, res) => {
 export const obtenerUsuario = async (req,res)=>{
   try {
       console.log(req.params.id)
-      const usuarioBuscado = await cliente.findById(req.params.id);
+      const usuarioBuscado = await usuario.findById(req.params.id);
       res.status(200).json(usuarioBuscado);
   } catch (error) {
       console.log(error);
@@ -81,7 +80,7 @@ export const editarUsuario = async (req, res)=>{
               errores:errores.array()
           })
       }
-      await cliente.findByIdAndUpdate(req.params.id,req.body);
+      await usuario.findByIdAndUpdate(req.params.id,req.body);
       res.status(200).json({
           mensaje:"El usuario fue actualizado con exito"
       })
@@ -95,7 +94,7 @@ export const editarUsuario = async (req, res)=>{
 
 export const borrarUsuario = async (req,res)=>{
   try {
-      await cliente.findByIdAndDelete(req.params.id);
+      await usuario.findByIdAndDelete(req.params.id);
       res.status(200).json({
           mensaje:"Usuario eliminado correctamente"
       })
--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -7,22 +7,27 @@ const usuarioRouter = Router();
 usuarioRouter.route("/usser")
 .get(listarUsuarios)
 .post([
-    check("Usuario","Usuario es un dato obligatorio"),
-    check("Contraseña","La contraseña debe contar minimo 8 caracteres")
-], login)
- 
-usuarioRouter.route("/usser/agregar")
-.post([
-    check("Nombre","El Nombre es un dato obligatorio"),
-    check("Usuario","Usuario es un dato obligatorio"),
-    check("Contraseña","La contraseña debe contar minimo 8 caracteres"),
-    check("Rol","El Rol es un dato obligatorio")
+    check("Nombre")
     .notEmpty()
     .withMessage("Este es un dato obligatorio")
-    .isIn(["Empleado","Maestro"])
-    .withMessage("Debe contener un estado")
+    .isLength({min: 2, max: 50})
+    .withMessage("Debe respetar las limitaciones de caracteres"),
+    check("Usuario")
+    .notEmpty()
+    .isLength({min: 1, max: 50})
+    .withMessage("Debe respetar las limitaciones de caracteres"),
+    check("Contraseña")
+    .notEmpty()
+    .withMessage("Este es un dato obligatorio")
+    .isLength({min: 5, max: 25})
+    .withMessage("Debe respetar las limitaciones de caracteres"),
+    check("Rol")
+    .notEmpty()
+    .withMessage("Este es un dato obligatorio")
+    .isIn(["Empleado", "Maestro"])
+    .isLength({min: 1, max: 50})
 ],agregarUsuario)
-
+ 
 usuarioRouter.route("/usser/:id")
 .put([
     check("Nombre")
@@ -32,23 +37,22 @@ usuarioRouter.route("/usser/:id")
     .withMessage("Debe respetar las limitaciones de caracteres"),
     check("Usuario")
     .notEmpty()
-    .isLength({min: 1, max: 12})
+    .isLength({min: 1, max: 50})
     .withMessage("Debe respetar las limitaciones de caracteres"),
     check("Contraseña")
     .notEmpty()
     .withMessage("Este es un dato obligatorio")
     .isLength({min: 5, max: 25})
     .withMessage("Debe respetar las limitaciones de caracteres"),
-
     check("Rol")
     .notEmpty()
     .withMessage("Este es un dato obligatorio")
     .isIn(["Empleado", "Maestro"])
     .isLength({min: 1, max: 50})
-    .withMessage("Debe respetar las limitaciones de caracteres")
-    
-], editarUsuario)
+],editarUsuario)
 .delete(borrarUsuario)
+
+
 
 
 export default usuarioRouter;
